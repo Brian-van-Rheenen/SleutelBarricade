107b94e87b7652d35650d1c1f3bf868bc21e2bfc
Lucas Spits <spits.lucas@gmail.com>
2019-03-26 15:28:37 +0100

    Manually implement drawing for the game grid

--- src/game/PlayingField.java
+++ src/game/PlayingField.java
@@ -2,29 +2,70 @@ package game;
 
 import game.objects.GameObject;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class PlayingField extends JPanel{
 
-    private int width;
-    private int height;
+    private List<GameObject> gameObjects;
+
+    private final int MAX_HEIGHT_CELL_AMOUNT = 10;
 
     public PlayingField(List<GameObject> GameObjects) {
         setLayout(new GridBagLayout());
 
-        GridBagConstraints gbc = new GridBagConstraints();
-        for (GameObject object: GameObjects) {
-            Position pos = object.getPosition();
-
-            gbc.gridx = pos.getxPosition();
-            gbc.gridy = pos.getyPosition();
-            add(object,gbc);
-        }
-
+        this.gameObjects = GameObjects;
     }
 
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        double[] sort = { getSize().getHeight(), getSize().getWidth() };
+        Arrays.sort(sort);
+
+        // Get the smallest possible element
+        double size = sort[0];
+
+        g.setColor(Color.black);
+
+        Number cellHeight = (size / (double) MAX_HEIGHT_CELL_AMOUNT);
 
 
+        for(int collumn = 0; collumn < cellHeight.intValue() * MAX_HEIGHT_CELL_AMOUNT; collumn = collumn + cellHeight.intValue()) {
+            // Draw a row
+            for(int row = 0; row < cellHeight.intValue() * MAX_HEIGHT_CELL_AMOUNT; row = row + cellHeight.intValue()) {
+
+                // Draw the cell
+                g.drawRect(collumn, row, cellHeight.intValue(), cellHeight.intValue());
+            }
+        }
+
+        // Draw all the different GameObjects on screen
+        for (GameObject gameObject : gameObjects) {
+
+            // Calculate the actual pixel position for the image
+            Position pos = gameObject.getPosition();
+
+            int actualX = cellHeight.intValue() * pos.getxPosition();
+            int actualY = cellHeight.intValue() * pos.getyPosition();
+
+            try {
+                // Load the image from the URL
+                Image image = ImageIO.read(gameObject.getUrl());
+
+                // Draw the Image on the screen, resize to the full size of the cell
+                g.drawImage(image, actualX, actualY, cellHeight.intValue(), cellHeight.intValue(), this);
+
+            } catch (IOException ex) {
+                // We were unable to load the image from the source
+                // Ignore the object and continue
+                continue;
+            }
+        }
+    }
 }
--- src/game/objects/GameObject.java
+++ src/game/objects/GameObject.java
@@ -11,8 +11,6 @@ public abstract class GameObject extends JPanel {
 
     private Position position;
     private URL url;
-    private ImageIcon imageIcon;
-    private JLabel label;
 
     public GameObject(Position position) {
         super();
@@ -25,27 +23,10 @@ public abstract class GameObject extends JPanel {
     }
 
     public void setImage(URL imagePath){
-        imageIcon = new ImageIcon(imagePath);
-        label = new JLabel(imageIcon);
-        add(label);
-
+        url = imagePath;
     }
 
-    // This portion of code was copied from a StackOverflow answer. This answer can be found via the following link:
-    // https://stackoverflow.com/questions/16075022/making-a-jpanel-square
-    @Override
-    public Dimension getPreferredSize() {
-        Dimension d = super.getPreferredSize();
-        Container c = getParent();
-        if (c != null) {
-            d = c.getSize();
-        } else {
-            return new Dimension(10, 10);
-        }
-        int w = (int) d.getWidth();
-        int h = (int) d.getHeight();
-        int s = (w < h ? w : h);
-        return new Dimension(s, s);
+    public URL getUrl() {
+        return url;
     }
-
 }
